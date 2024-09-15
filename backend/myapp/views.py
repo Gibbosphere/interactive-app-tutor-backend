8300d0c6f88f86c0688e4d38d8117c10abf23d16
gbskan001 <gbskan001@cs.uct.ac.za>
2024-09-15 02:07:51 +0200

    Fixed api duplicate bugs

--- backend/myapp/views.py
+++ backend/myapp/views.py
@@ -4,22 +4,22 @@ from .models import InteractiveGuide, TooltipInteractiveGuide, TooltipTutorial, 
 from .serializers import InteractiveGuideSerializer, InfoIconSerializer, TutorialStageSerializer, DocumentationPageSerializer
 
 class InteractiveGuideViewSet(viewsets.ModelViewSet):
-    queryset = InteractiveGuide.objects.all()
+    queryset = InteractiveGuide.objects.prefetch_related('tooltips').distinct()
     serializer_class = InteractiveGuideSerializer
     filter_backends = [OrderingFilter]  # Add ordering filter
-    ordering_fields = ['tooltips__position']  # Define ordering field
-    ordering = ['tooltips__position']  # Apply default ordering by tooltips position
+    # ordering_fields = ['tooltips__position']  # Define ordering field
+    # ordering = ['tooltips__position']  # Apply default ordering by tooltips position
 
 class InfoIconViewSet(viewsets.ModelViewSet):
     queryset = InfoIcon.objects.all()
     serializer_class = InfoIconSerializer
 
 class TutorialViewSet(viewsets.ModelViewSet):
-    queryset = TutorialStage.objects.all()
+    queryset = TutorialStage.objects.prefetch_related('tooltips', 'test_tasks__click_elements').distinct()
     serializer_class = TutorialStageSerializer
     filter_backends = [OrderingFilter]
-    ordering_fields = ['tooltips__position', 'test_tasks__position', 'test_tasks__click_elements__position']
-    ordering = ['tooltips__position', 'test_tasks__position', 'test_tasks__click_elements__position']  # Apply ordering using the position value
+    # ordering_fields = ['tooltips__position', 'test_tasks__position', 'test_tasks__click_elements__position']
+    # ordering = ['tooltips__position', 'test_tasks__position', 'test_tasks__click_elements__position']  # Apply ordering using the position value
 
 class DocumentationViewSet(viewsets.ModelViewSet):
     queryset = DocumentationPage.objects.all()
